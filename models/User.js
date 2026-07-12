f81f4a90807e908c3a6c6242bb7cdb567ea675e1
Shruti-kri08 <shrutiikumarii08@gmail.com>
2026-07-12 14:15:21 +0530

    add phone attribute

--- models/User.js
+++ models/User.js
@@ -17,6 +17,11 @@ const userSchema = new mongoose.Schema({
     type: String,
     minlength: 8,
     required: true
+  },
+   phone: {
+    type: String,
+    minlength: 8,
+    required: true
   }
 }, {
   timestamps: true
