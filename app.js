85b2fd2a727690a5366f086579a09696436af039
Shruti-kri08 <shrutiikumarii08@gmail.com>
2026-07-24 23:05:37 +0530

    change6

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 const express = require('express')
 const app = express()
-const app = express();
-
 app.set("trust proxy", 1);
 const cors = require('cors');
 const mongoose = require('mongoose')
